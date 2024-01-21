e48888eb654ae70d2fae4b2a844836717dc28b67
burtlau <bert.lau@outlook.com>
2024-01-21 04:58:03 -0500

    m 1.20 finish reading chap5

---
+++ Java_learning/core_java/codes/src/main/java/chap5/EnumTest.java
@@ -0,0 +1,25 @@
+package chap5;
+import java.util.*;
+
+public class EnumTest {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a size : (SMALL / MEDIUM/ LARGE/ EXTRA_LARGE)");
+        String input = in.next().toUpperCase();
+        Size size = Enum.valueOf(Size.class, input);
+        System.out.println("size=" + size);
+        System.out.println("abbreviation=" + size.getAbbrieviation());
+        if (size == Size.EXTRA_LARGE)
+            System.out.println("Good job -- you paid attention to the -.");
+    }
+}
+
+enum Size{
+    SMALL("S"), MEDIUM("M"), LARGE("L"), EXTRA_LARGE("XL");
+
+    private Size(String abbreviation) { this.abbrieviation = abbreviation; }
+    public String getAbbrieviation(){
+        return abbrieviation;
+    }
+    private String abbrieviation;
+}
