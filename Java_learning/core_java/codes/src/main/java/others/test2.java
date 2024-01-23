e4d392d3bf5f1bf12587eedf28800c0eefa77c62
burtlau <bert.lau@outlook.com>
2024-01-23 01:38:13 -0500

    1.21 did 2 test case

---
+++ Java_learning/core_java/codes/src/main/java/others/test2.java
@@ -0,0 +1,20 @@
+package others;
+
+public class test2 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 2;
+        try{
+            for (int z = 2 ; z >= 0; z--)
+            {
+                int ans = x / z;
+                System.out.println(ans + " ");
+            }
+        } catch (ArithmeticException e1) {
+            System.out.println("E2");
+        } catch (Exception e1) {
+            System.out.println("E1");
+        } 
+
+    }
+}
